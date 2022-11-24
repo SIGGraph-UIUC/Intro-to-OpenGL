a2ad0a1ef81752da09aeb28d1024dbe76508466b
Jeffrey Liu <jeffliu604@gmail.com>
2022-11-24 01:43:37 -0500

    Hello triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,34 @@ void framebuffer_resize(GLFWwindow* window, int new_width, int new_height)
     glViewport(0, 0, new_width, new_height);
 }
 
+const std::string vertex_path = "C:\\Users\\JeffL\\Desktop\\Intro-to-OpenGL\\shaders\\vertex.glsl";
+const std::string fragment_path = "C:\\Users\\JeffL\\Desktop\\Intro-to-OpenGL\\shaders\\fragment.glsl";
+
+unsigned int create_triangle()
+{
+    const float vertices[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f};
+    const int indices[] = { 0, 1, 2 };
+
+    unsigned int vao = 0;
+    glCreateVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    unsigned int buffer = 0;
+    glCreateBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    unsigned int index_buffer = 0;
+    glCreateBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    return vao;
+}
+
 int main()
 {
     // initialize and configure GLFW
@@ -39,12 +67,18 @@ int main()
     glViewport(0, 0, width, height);
     glfwSetFramebufferSizeCallback(window, framebuffer_resize);
 
-    int shader_program = create_program("", "");
+    int shader_program = create_program(vertex_path, fragment_path);
+    unsigned int vao = create_triangle();
 
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
